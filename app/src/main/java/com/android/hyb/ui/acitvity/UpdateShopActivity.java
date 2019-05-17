6be5eeccae63db96a6a6b066828edc7f617c9e02
David <yangshangwei@yonrun.com>
2019-05-17 10:57:41 +0800

    上传商品

---
+++ app/src/main/java/com/android/hyb/ui/acitvity/UpdateShopActivity.java
@@ -0,0 +1,30 @@
+package com.android.hyb.ui.acitvity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.hyb.R;
+import com.android.hyb.base.BaseActivity;
+
+public class UpdateShopActivity extends BaseActivity {
+
+    @Override
+    public int setViewId() {
+        return R.layout.activity_update_shop;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
